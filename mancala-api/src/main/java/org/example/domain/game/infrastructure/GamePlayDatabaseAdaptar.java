3c471b32fa546a97bc5d7df42494066586d81d2f
Nishi <nishi.mailbox@gmail.com>
2021-03-06 22:03:15 +0100

    changes for cacheing

--- mancala-api/src/main/java/org/example/domain/game/infrastructure/GamePlayDatabaseAdaptar.java
+++ mancala-api/src/main/java/org/example/domain/game/infrastructure/GamePlayDatabaseAdaptar.java
@@ -13,19 +13,19 @@ public class GamePlayDatabaseAdaptar implements GamePlayDatabase {
     private final GamePlayRepository gamePlayRepository;
 
     @Override
-    //@CachePut(value = "mancala", key = "#gameState.gameId")
+    @CachePut(value = "mancala", key = "#gameState.gameId")
     public GameState save(GameState gameState) {
         return gamePlayRepository.save(gameState);
     }
 
     @Override
-    //@Cacheable(value = "mancala", key = "#gameId" , unless = "#result  == null")
+    @Cacheable(value = "mancala", key = "#gameId" , unless = "#result  == null")
     public GameState load(final String gameId) {
         return gamePlayRepository.findById(gameId).orElseThrow(() -> new StorageNotFoundException("Unknown Game"));
     }
 
     @Override
-    //@CachePut(value = "mancala", key = "#updatedGameState.gameId" , unless = "#result  == null")
+    @CachePut(value = "mancala", key = "#updatedGameState.gameId" , unless = "#result  == null")
     public GameState update(GameState updatedGameState) {
         return gamePlayRepository.save(updatedGameState);
     }
