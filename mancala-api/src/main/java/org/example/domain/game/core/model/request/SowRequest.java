19e00077df69f19711561e0f5c20147f29755b14
Nishi <nishi.mailbox@gmail.com>
2021-03-14 19:04:09 +0100

    changes for test case

--- mancala-api/src/main/java/org/example/domain/game/core/model/request/SowRequest.java
+++ mancala-api/src/main/java/org/example/domain/game/core/model/request/SowRequest.java
@@ -2,10 +2,12 @@ package org.example.domain.game.core.model.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class SowRequest {
-    private final String pickPosition;
-    private final String movingPlayerId;
+    private String pickPosition;
+    private String movingPlayerId;
 }
