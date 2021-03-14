f469344faad6b2756c276324d1ba9f4ff8ae5b31
Nishi <nishi.mailbox@gmail.com>
2021-03-14 15:44:21 +0100

    changes for adding tests

--- mancala-api/src/test/java/org/example/domain/player/application/PlayerControllerTest.java
+++ mancala-api/src/test/java/org/example/domain/player/application/PlayerControllerTest.java
@@ -1,8 +1,9 @@
 package org.example.domain.player.application;
 
 import org.example.domain.player.core.model.command.AddPlayerCommand;
+import org.example.domain.player.core.model.command.GetPlayerCommand;
 import org.example.domain.player.core.model.output.PlayerIdentifier;
-import org.example.domain.player.core.ports.PlayerFacade;
+import org.example.domain.player.core.model.output.PlayerResponse;
 import org.example.domain.player.core.ports.incoming.AddNewPlayer;
 import org.example.domain.player.core.ports.incoming.GetPlayer;
 import org.junit.jupiter.api.Test;
@@ -16,13 +17,14 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = "server.port=0")
 @DirtiesContext
-public class PlayerControllerTest extends ControllerTest{
+public class PlayerControllerTest extends ControllerTest {
 
     @MockBean
     private AddNewPlayer addNewPlayer;
@@ -52,16 +54,65 @@ public class PlayerControllerTest extends ControllerTest{
     public void testPlayerCreationWhenErrorFromFacade() throws Exception {
 
         Mockito.when(this.addNewPlayer.add(any(AddPlayerCommand.class)))
-                .thenReturn(new PlayerIdentifier("SomeValue"));
+                .thenThrow(new IllegalArgumentException("Some Error happened"));
 
         this.mockMvc.perform(post("/player")
                 .content(asJsonString(new AddPlayerCommand("firstName4", "lastName4")))
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().is2xxSuccessful())
+                .andExpect(status().is5xxServerError())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.playerId").value("SomeValue"))
+                .andExpect(jsonPath("$.message").exists())
+                .andExpect(jsonPath("$.errorTime").exists())
                 .andReturn();
     }
+
+    @Test
+    public void testGetPlayerWhenExistingPlayerId() throws Exception {
+
+        Mockito.when(this.getPlayer.get(any(GetPlayerCommand.class))).thenReturn(new PlayerResponse("somevalue","somevalue"));
+
+        this.mockMvc.perform(get("/player/{id}","id8475785")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.firstName").exists())
+                .andExpect(jsonPath("$.lastName").exists())
+                .andReturn();
+
+    }
+
+    @Test
+    public void testGetPlayerWhenNonExistingPlayerId() throws Exception {
+
+        Mockito.when(this.getPlayer.get(any(GetPlayerCommand.class)))
+                .thenReturn(null);
+
+        this.mockMvc.perform(get("/player/{id}","tttttt")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound())
+                .andReturn();
+
+    }
+
+    @Test
+    public void testGetPlayerWhenErrorThrown() throws Exception {
+
+        Mockito.when(this.getPlayer.get(any(GetPlayerCommand.class)))
+                .thenThrow(new IllegalArgumentException("Invalid player"));
+
+        this.mockMvc.perform(get("/player/{id}","tttttt")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().is5xxServerError())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.message").exists())
+                .andExpect(jsonPath("$.errorTime").exists())
+                .andReturn();
+
+    }
+
 
 }
